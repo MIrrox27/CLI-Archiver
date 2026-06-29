361ea87d8c64354fc6ebe81372e1c3f310e51bd9
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-06-29 20:56:47 +0500

    добавлена обработка аргументов

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <map> 
 #include <functional>
+#include <vector>
 #include "encoder.hpp"
 
   // Скелеты функций
@@ -12,16 +13,21 @@ void encode_archive(std::string path);
 void decode_archive(std::string path);
 
 
-int main() {
+int main(int argc, char* argv[]) {
   using Callback = std::function<void(std::string)>; // Тип данных для функций
 
   std::map<std::string, Callback> tags;
   tags["-c"] = [](std::string path) { encode_archive(path); };
   tags["-d "] = [](std::string path) { decode_archive(path); };
 
-  std::string tag = __argv[1];
-  std::string path = __argv[2];
-  std::string archive_name = __argv[3];
+  std::cout << argc << std::endl;
+  std::cout << argv[1] << std::endl;
+  std::cout << argv[2] << std::endl;
+  std::cout << argv[3] << std::endl;
+    
+  std::string tag = argv[1];
+  std::string path = argv[2];
+  std::string archive_name = argv[3];
   
   if (tags.find(tag) != tags.end()) tags[tag](path);
   else std::cerr << "Invalid arg: " << tag << std::endl;
@@ -33,5 +39,9 @@ int main() {
 
 
 
-void encode_archive(std::string path){}; // функция для создания архива
+void encode_archive(std::string path){ // функция для создания архива
+  //std::vector<char> result = get_binary_file(path);
+  std::cout << "encode_archive " << std::endl;
+}; 
+
 void decode_archive(std::string path){}; // функция для распаковки архива 
