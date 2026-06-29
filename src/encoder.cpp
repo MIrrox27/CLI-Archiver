f5c80a79b54eb4692d8648b1a4cecb6eb7559bda
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-06-29 20:56:27 +0500

    добавлена функция для перевода файла в бинарный формат encoder.hpp

--- src/encoder.cpp
+++ src/encoder.cpp
@@ -3,5 +3,20 @@
 
 #include <fstream>
 #include <vector>
+#include <iostream>
 
-  
+  // Получение бинарной информации из файла
+std::vector<char> get_binary_file(std::string path) {
+
+  std::ifstream file(path, std::ios::binary);
+  if (!file.is_open()) {
+    std::cerr << "Invalid path: " << path << std::endl;
+    return {};
+  }
+  else {
+    std::vector<char> binary_buffer((std::istreambuf_iterator<char>(file)),
+                                  std::istreambuf_iterator<char>());
+    file.close();
+    return binary_buffer;
+  }
+}
