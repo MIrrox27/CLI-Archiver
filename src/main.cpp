936a4a415164fc50f83d492ce46206f751ac4cce
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-06-29 18:30:04 +0500

    добавлены функции для кодирования и декодирования архива

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,31 @@
 // author github.com/MIrrox27/CLI-Archiver
 // src/main.cpp
+
+#include <iostream>
+#include <string>
+#include <map> 
+#include <functional>
+#include "encoder.hpp"
+
+  // Скелеты функций
+void encode_archive(std::string path);
+void decode_archive(std::string path);
+
+
+int main() {
+  using Callback = std::function<void(std::string)>; // Тип данных для функций
+
+  std::map<std::string, Callback> tags;
+  tags["-c"] = [](std::string path) { encode_archive(path); };
+  tags["-d "] = [](std::string path) { decode_archive(path); };
+
+
+  
+  std::cout << " -- Pragramm ended without errors..." << std::endl;
+  return 0;
+}
+
+
+
+void encode_archive(std::string path){}; // функция для создания архива
+void decode_archive(std::string path){}; // функция для распаковки архива 
