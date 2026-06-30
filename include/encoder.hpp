7853d126b8e6a575325b47c5223c260b046b0ceb
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-06-30 14:57:15 +0500

    обновлены параметры

--- include/encoder.hpp
+++ include/encoder.hpp
@@ -10,6 +10,7 @@
 
 std::vector<char> get_binary_file(std::string path);
 std::vector<unsigned char> encode_binary(std::vector<char> binary_file);
+int wright_encode_to_file(std::vector<unsigned char> compressed, std::string name);
 
 
 
