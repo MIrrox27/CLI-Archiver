0c5ba44bbc70dfac44bce1f8327c646bd059141e
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-07-01 11:25:00 +0500

    изменено имя функции

--- include/decoder.hpp
+++ include/decoder.hpp
@@ -8,7 +8,7 @@
 #include <vector>
 
 
-std::vector<char> get_encode_file(std::string path);
+std::vector<char> get_binary_archive(std::string path);
 std::vector<unsigned char> decode_binary(std::vector<char> binary);
 int wright_decode_to_file (std::vector<unsigned char> compressed, std::string name);
 
